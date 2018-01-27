8059193f6e38552d5a01871e5c0638b631915560
PrzemShien <przemyslaw.postek@gmail.com>
2018-01-27 15:09:33 +0100

    empty commit

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+
+
+var modal = document.querySelectorAll('.modal');
+console.log(modal);
+
+var img = document.querySelectorAll('.myImg');
+console.log(img);
+
+var modalImg = document.querySelectorAll('.modalImg');
+for(let i = 0; i < img.length; i ++){
+    img[i].addEventListener('click', function(){
+        modal[i].style.display = "block";
+        modalImg[i].src = this.src;
+    }, true);
+}
+
+var span = document.querySelectorAll('.close');
+for(let i = 0; i < span.length; i++){
+span[i].addEventListener('click', function(){
+    modal[i].style.display = "none";
+}, true);
+}
